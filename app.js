b146f85274bce6bede9c5a776e761e1024f0be50
Gagandeep Singh <gagandeepsingh516@ymail.com>
2021-11-09 14:09:12 +0530

    Second Commit

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const cors = require("cors");
 const app = express();
 
-app.get('/',(req,res)=>{
-     res.send('Welcome to the home page');
-})
+app.get("/no-cors", (req, res) => {
+    console.info("GET /no-cors");
+    res.json({
+      text: "You should not see this via a CORS request."
+    });
+  });
+
+app.get("/simple-cors", cors(), (req, res) => {
+    res.json({
+        text: "Simple CORS requests are working. [GET]"
+      });
+  });
 
 const port = process.env.port || 3000;
 app.listen(port,() =>{
